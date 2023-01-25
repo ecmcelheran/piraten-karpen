98da952f6f84639869157316cade7cfbf04b3f86
ecmcelheran <ecmcelheran15@gmail.com>
2023-01-25 10:50:05 -0500

    F03

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -3,8 +3,30 @@ import pk.Simulation;
 public class PiratenKarpen {
 
     public static void main(String[] args) {
-        //use first command line argument as number of games
-        Simulation sim = new Simulation(42, args);
+        //command line arguments determine mode (i.e. trace mode), number of games and player strategies
+        //args mode, games, strategies
+        try {
+            if (args.length == 4) {
+                String[] strategies = {args[2], args[3]};
+                Simulation sim = new Simulation(args[0], Integer.parseInt(args[1]), strategies);
+            } else if (args.length == 3) {
+                String[] strategies = {args[1], args[2]};
+                Simulation sim = new Simulation("fatal", Integer.parseInt(args[0]), strategies);
+
+            } else if (args.length == 2) {
+                Simulation sim = new Simulation("fatal", 42, args);
+            } else {
+                String arr[] = new String[0];
+                try {
+                    Simulation sim = new Simulation("fatal", Integer.parseInt(args[0]), arr);
+                } catch (Exception e) {
+                    Simulation sim = new Simulation(args[0], 42, arr);
+                }
+            }
+        }catch(Exception e) {
+            String arr[] = new String[0];
+            Simulation sim = new Simulation("fatal", 42, arr);
+        }
 
     }
 }
--- src/main/java/pk/Simulation.java
+++ src/main/java/pk/Simulation.java
@@ -8,19 +8,28 @@ import org.apache.logging.log4j.core.config.Configurator;
 public class Simulation {
     private static final Logger logger = LogManager.getLogger(Simulation.class);
 
-    int[] wins = {0,0};
-    Player[] player = new Player[2];
+    int[] wins;
+    Player[] player;
     //game comes with 8 dice
     Dice[] mydice = new Dice[8];
 
     int games;
 
-
-    public Simulation(int games, String[] args){
-        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.TRACE);
+    public Simulation(String mode, int games, String... args){
         System.out.println("Welcome to Piraten Kapern!");
-
+        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.getLevel(mode.toUpperCase()));
         this.games = games;
+        if(args.length>1){
+            player = new Player[args.length];
+        }
+        else{
+            player = new Player[2];
+        }
+
+        wins = new int[player.length];
+        for(int w: wins){
+            w=0;
+        }
 
         //instantiate dice
         for(int i=0; i<mydice.length; i++){
@@ -41,26 +50,41 @@ public class Simulation {
             player[0].reset();
             player[1].reset();
         }
-        System.out.printf("Player 1 won %1.2f%% of games.\nPlayer 2 won %1.2f%% of games.",wins[0]*100.0/42.0,wins[1]*100.0/42.0);
+        for(int i=0; i<player.length;i++){
+            System.out.printf("Player " + (i+1) + " won %1.2f%% of games.\n", wins[i]*100.0/games);
+        }
     }
 
     public void playGame(){
-        //game continues as long as player scores are lower than 6000
-        while(player[0].gameContinue() && player[1].gameContinue()){
+        int winner = 0;
+        boolean gameContinue = true;
 
-            logger.info("-PLAYER 1-\n");
-            player[0].playTurn(mydice);
-            logger.info("-PLAYER 2-\n");
-            player[1].playTurn(mydice);
+        //game continues as long as player scores are lower than 6000
+        while(gameContinue){
+
+            for(int i=0; i<player.length; i++){
+                logger.debug("Player " + (i+1) + "'s turn");
+                player[i].playTurn(mydice);
+            }
+
+            //check player scores
+            for(Player p: player){
+                if(!p.gameContinue()){
+                    gameContinue = false;
+                    break;
+                }
+            }
 
             logger.info("Player 1 score: "+ player[0].showScore()+"\nPlayer 2 score: "+ player[1].showScore());
         }
 
-        if(player[0].getScore()>player[1].getScore()){
-            wins[0]++;
+        //check winner
+        for(int i=0; i<player.length; i++){
+            if(player[i].getScore()>player[winner].getScore()){
+                winner = i;
+            }
         }
-        else{
-            wins[1]++;
-        }
+
+        wins[winner]++;
     }
 }
